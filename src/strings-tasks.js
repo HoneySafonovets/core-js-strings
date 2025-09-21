4a35dbcdaef62d86d689e077f58f1b0e76ae0b0d
HoneySafonovets <kfs.boi@yandex.ru>
2025-09-21 20:49:45 +0300

    feat: add function sumOfCodes

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -196,8 +196,15 @@ function removeLastOccurrences(str, value) {
  *   sumOfCodes('') => 0
  *   sumOfCodes() => 0
  */
-function sumOfCodes(/* str */) {
-  throw new Error('Not implemented');
+function sumOfCodes(str) {
+  if (typeof str === 'string') {
+    let subSum = 0;
+    for (let i = 0; i < str.length; i += 1) {
+      subSum += str.charCodeAt(i);
+    }
+    return subSum;
+  }
+  return 0;
 }
 
 /**
