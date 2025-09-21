343ee401d06770b8022675da54430643e538cdb1
HoneySafonovets <kfs.boi@yandex.ru>
2025-09-21 20:26:59 +0300

    feat: add function removeLastOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -177,8 +177,11 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  if (str.lastIndexOf(value) === -1) {
+    return str;
+  }
+  return `${str.slice(0, str.lastIndexOf(value))}${str.slice(str.lastIndexOf(value) + value.length)}`;
 }
 
 /**
